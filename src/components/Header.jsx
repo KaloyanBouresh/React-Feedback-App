35d66ebfbdea89de6f677a76c525600a4a229649
KaloyanBouresh <kalata2316@gmail.com>
2022-10-10 17:14:47 +0300

    remove import React from react

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import PropTypes from 'prop-types' //define the type of the props
 
 function Header({ text }) {
--- src/components/RatingSelect.jsx
+++ src/components/RatingSelect.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { useState, useContext, useEffect } from "react"
 import FeedbackContext from "../context/FeedbackContext"
 
